3501b47eaea84dacaa2aeac8d2602caffbde34a5
Shafaq Arefin Chowdhury <shafaqarefinchowdhury@gmail.com>
2025-02-02 03:20:23 +0600

    UI logic for checkbox

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,12 +1,24 @@
-export function Items({ items, key }) {
+export function Items({ items, onDeleteItem, handleChecked }) {
   return (
     <li>
-      <input type="checkbox" id={key} />
-      <span>
+      <input
+        type="checkbox"
+        id={items.id}
+        onClick={(e) => {
+          handleChecked(Number(e.target.id), e.target.checked);
+        }}
+      />
+      <span style={items.isPacked ? { textDecoration: "line-through" } : {}}>
         {items.quantity + " "}
         {items.description}
       </span>
-      <button>❌</button>
+      <button
+        onClick={(e) => {
+          onDeleteItem(items.id);
+        }}
+      >
+        ❌
+      </button>
     </li>
   );
 }
