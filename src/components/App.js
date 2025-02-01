c0a2966f3027fc18af1f652d8af6cbe116fc54e8
Shafaq Arefin Chowdhury <shafaqarefinchowdhury@gmail.com>
2025-02-02 03:18:26 +0600

    Added delete,checkbox functionalities and dyanmic UI update

--- src/components/App.js
+++ src/components/App.js
@@ -2,13 +2,40 @@ import { Logo } from "./Logo.js";
 import { PackingList } from "./Packinglist.js";
 import { Stats } from "./Stat.js";
 import { AddForm } from "./Form.js";
+import { useState } from "react";
 
 function App() {
+  const [item, setItem] = useState([]);
+  function onAddItem(newItem) {
+    setItem([...item, newItem]);
+  }
+  function onClearList() {
+    setItem([]);
+  }
+  function onDeleteItem(deleteId) {
+    const updatedItems = item.filter((vals) => vals.id !== deleteId);
+    setItem(updatedItems);
+  }
+  function handleChecked(id, checked) {
+    const updatedItems = item.map((items) =>
+      items.id === id && checked
+        ? { ...items, isPacked: true }
+        : { ...items, isPacked: false }
+    );
+
+    setItem(updatedItems);
+  }
+
   return (
     <div className="app">
       <Logo />
-      <AddForm />
-      <PackingList />
+      <AddForm onAddItem={onAddItem} item={item} />
+      <PackingList
+        itemsList={item}
+        onClearList={onClearList}
+        onDeleteItem={onDeleteItem}
+        handleChecked={handleChecked}
+      />
       <Stats />
     </div>
   );
