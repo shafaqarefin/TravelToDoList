51455b72eb0206e84ef5c3b9bc0aff77e0499a86
Shafaq Arefin Chowdhury <shafaqarefinchowdhury@gmail.com>
2025-02-02 03:21:02 +0600

    clear list functionality,added event handlers

--- src/components/Packinglist.js
+++ src/components/Packinglist.js
@@ -1,16 +1,42 @@
 import { Items } from "./Item.js";
-import * as Form from "./Form.js";
-import { travelList } from "../db.js";
-export function PackingList() {
+// import * as Form from "./Form.js";
+// import { travelList } from "../db.js";
+export function PackingList({
+  itemsList,
+  onClearList,
+  onDeleteItem,
+  handleChecked,
+}) {
+  function arrangeList(e) {}
+
   return (
     <>
       <div className="list">
         <ul>
-          {travelList.map((items) => (
-            <Items items={items} key={items.id} />
+          {itemsList.map((items) => (
+            <Items
+              items={items}
+              onDeleteItem={onDeleteItem}
+              handleChecked={handleChecked}
+            />
           ))}
         </ul>
-        <Form.ActionForm />
+        <div className="actions">
+          <select onChange={arrangeList}>
+            <option value="input">SORT BY INPUT ORDER</option>
+            <option value="description">SORT BY DESCRIPTION </option>
+            <option value="packed">SORT BY PACKED STATUS</option>
+          </select>
+          <button
+            onClick={() => {
+              if (window.confirm("Are you sure about that?")) {
+                onClearList();
+              }
+            }}
+          >
+            CLEAR LIST
+          </button>
+        </div>
       </div>
     </>
   );
