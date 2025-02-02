0daea191fb106e5d93ef43f33da3923647f9bbb1
Shafaq Arefin Chowdhury <shafaqarefinchowdhury@gmail.com>
2025-02-02 21:57:47 +0600

    added work done stats functionality,fixed toggle checkbox issue

--- src/components/App.js
+++ src/components/App.js
@@ -6,6 +6,11 @@ import { useState } from "react";
 
 function App() {
   const [item, setItem] = useState([]);
+  const numberOfItems = item.length;
+  const numberOfPacked = item.filter((items) => items.isPacked).length;
+  const percentageCompleted = Math.round(
+    (numberOfPacked / numberOfItems) * 100
+  );
   function onAddItem(newItem) {
     setItem([...item, newItem]);
   }
@@ -14,15 +19,13 @@ function App() {
   }
   function onDeleteItem(deleteId) {
     const updatedItems = item.filter((vals) => vals.id !== deleteId);
+
     setItem(updatedItems);
   }
   function handleChecked(id, checked) {
     const updatedItems = item.map((items) =>
-      items.id === id && checked
-        ? { ...items, isPacked: true }
-        : { ...items, isPacked: false }
+      items.id === id ? { ...items, isPacked: checked } : items
     );
-
     setItem(updatedItems);
   }
 
@@ -36,7 +39,11 @@ function App() {
         onDeleteItem={onDeleteItem}
         handleChecked={handleChecked}
       />
-      <Stats />
+      <Stats
+        numberOfItems={numberOfItems}
+        numberOfPacked={numberOfPacked}
+        percentageCompleted={percentageCompleted}
+      />
     </div>
   );
 }
