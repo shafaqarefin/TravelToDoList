ddf61b231d5c6107013629f68287ad3bf43ad43a
Shafaq Arefin Chowdhury <shafaqarefinchowdhury@gmail.com>
2025-02-02 22:00:40 +0600

    corrected event listener,added checked props to input to fix bug

--- src/components/Item.js
+++ src/components/Item.js
@@ -4,8 +4,9 @@ export function Items({ items, onDeleteItem, handleChecked }) {
       <input
         type="checkbox"
         id={items.id}
-        onClick={(e) => {
-          handleChecked(Number(e.target.id), e.target.checked);
+        checked={items.isPacked ? true : false}
+        onChange={(e) => {
+          handleChecked(e.target.id, e.target.checked);
         }}
       />
       <span style={items.isPacked ? { textDecoration: "line-through" } : {}}>
